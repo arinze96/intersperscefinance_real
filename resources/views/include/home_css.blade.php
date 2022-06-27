51250f395d6e5d2c7e2f15e315863f160c1e186e
Arinzechukwu Edmund <edmund10arinze@gmail.com>
2022-06-27 17:21:04 +0100

    first commit

--- resources/views/include/home_css.blade.php
+++ resources/views/include/home_css.blade.php
@@ -275,17 +275,16 @@
 
 </style>
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/62b98f6c7b967b117996af4d/1g6idgg57';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-    </script>
-    <!--End of Tawk.to Script-->
+
+							<!-- Smartsupp Live Chat script -->
+                            <script type="text/javascript">
+                                var _smartsupp = _smartsupp || {};
+                                _smartsupp.key = '6ba180c51557e213b484bd3489130e563acee5d2';
+                                window.smartsupp||(function(d) {
+                                  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+                                  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+                                  c.type='text/javascript';c.charset='utf-8';c.async=true;
+                                  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+                                })(document);
+                                </script>
     
